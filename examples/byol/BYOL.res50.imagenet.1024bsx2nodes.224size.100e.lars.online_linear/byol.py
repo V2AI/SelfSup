4b95bab93782dedd2643590569025526379a5fcb
Benjin Zhu <zhubenjin@megvii.com>
2021-04-29 17:43:12 +0800

    add training with online linear classifier example

---
+++ examples/byol/BYOL.res50.imagenet.1024bsx2nodes.224size.100e.lars.online_linear/byol.py
@@ -0,0 +1,143 @@
+import math
+import torch
+import torch.nn as nn
+
+from torch.nn import functional as F
+
+from cvpods.layers import ShapeSpec
+from cvpods.layers.metrics import accuracy
+
+
+class EncoderWithProjection(nn.Module):
+    def __init__(self, cfg):
+        super(EncoderWithProjection, self).__init__()
+        self.proj_dim = cfg.MODEL.BYOL.PROJ_DIM
+        self.out_dim = cfg.MODEL.BYOL.OUT_DIM
+
+        self.encoder = cfg.build_backbone(
+            cfg, input_shape=ShapeSpec(channels=len(cfg.MODEL.PIXEL_MEAN)))
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.projector = nn.Sequential(
+            nn.Linear(2048, self.proj_dim),
+            nn.BatchNorm1d(self.proj_dim),
+            nn.ReLU(),
+            nn.Linear(self.proj_dim, self.out_dim, bias=False),
+        )
+
+    def forward(self, x):
+        embedding = torch.flatten(self.avgpool(self.encoder(x)["res5"]), 1)
+        return self.projector(embedding)
+
+
+class BYOL(nn.Module):
+    def __init__(self, cfg):
+        super(BYOL, self).__init__()
+
+        self.device = torch.device(cfg.MODEL.DEVICE)
+        self.base_mom = cfg.MODEL.BYOL.BASE_MOMENTUM
+        self.total_steps = cfg.SOLVER.LR_SCHEDULER.MAX_ITER * \
+            cfg.SOLVER.BATCH_SUBDIVISIONS
+
+        self.online_network = EncoderWithProjection(cfg)
+        self.target_network = EncoderWithProjection(cfg)
+
+        encoder_dim = self.online_network.linear.weight.shape[1]
+        self.linear_eval = nn.Linear(encoder_dim, 1000)
+        self.loss_evaluator = nn.CrossEntropyLoss()
+
+        self.size_divisibility = self.online_network.encoder.size_divisibility
+
+        self.predictor = nn.Sequential(
+            nn.Linear(
+                self.online_network.out_dim,
+                self.online_network.proj_dim
+            ),
+            nn.BatchNorm1d(self.online_network.proj_dim),
+            nn.ReLU(),
+            nn.Linear(
+                self.online_network.proj_dim,
+                self.online_network.out_dim,
+                bias=False
+            ),
+        )
+
+        for param_q, param_k in zip(
+                self.online_network.parameters(),
+                self.target_network.parameters()):
+            param_k.data.copy_(param_q.data)  # initialize
+            param_k.requires_grad = False  # not update by gradient
+
+        self.register_parameter("step", nn.Parameter(torch.zeros(1), requires_grad=False))
+        self.register_parameter("mom", nn.Parameter(torch.zeros(1), requires_grad=False))
+
+        self.to(self.device)
+
+    def losses(self, preds, targets):
+        bz = preds.size(0)
+        preds_norm = F.normalize(preds, dim=1)
+        targets_norm = F.normalize(targets, dim=1)
+        loss = 2 - 2 * (preds_norm * targets_norm).sum() / bz
+        return loss
+
+    def update_mom(self):
+        mom = 1 - (1 - self.base_mom) * (math.cos(math.pi * self.step.item() / self.total_steps) + 1) / 2.
+        self.step += 1
+        return mom
+
+    @torch.no_grad()
+    def _momentum_update_key_encoder(self):
+        """
+        Momentum update of the key encoder
+        """
+        mom = self.update_mom()
+        self.mom[0] = mom
+        for param_q, param_k in zip(
+                self.online_network.parameters(),
+                self.target_network.parameters()):
+            param_k.data = param_k.data * mom + param_q.data * (1. - mom)
+
+    def forward(self, batched_inputs):
+        """
+        Input:
+            im_q: a batch of query images
+            im_k: a batch of key images
+        Output:
+            logits, targets
+        """
+        # Linear Evaluation
+        linear_inputs = [bi['linear'] for bi in batched_inputs]
+        x_linear = torch.stack([bi["image"] for bi in linear_inputs]).to(self.device)
+        logits = self.linear_eval(torch.flatten(
+            self.avgpool(self.online_network(x_linear)["res5"].detach()), 1))
+        labels = torch.tensor([gi["category_id"] for gi in linear_inputs]).cuda()
+        linear_loss = self.loss_evaluator(logits, labels)
+        acc1, acc5 = accuracy(logits, labels, topk=(1, 5))
+
+        # BYOL training
+        q_inputs = [bi["q"] for bi in batched_inputs]
+        k_inputs = [bi["k"] for bi in batched_inputs]
+
+        x_i = torch.stack([bi["image"][0] for bi in q_inputs]).to(self.device)
+        x_j = torch.stack([bi["image"][0] for bi in k_inputs]).to(self.device)
+
+        online_out_1 = self.predictor(self.online_network(x_i))
+        online_out_2 = self.predictor(self.online_network(x_j))
+
+        with torch.no_grad():
+            self._momentum_update_key_encoder()
+
+            target_out_1 = self.target_network(x_i)
+            target_out_2 = self.target_network(x_j)
+
+        loss_i = self.losses(online_out_1, target_out_2)
+        loss_j = self.losses(online_out_2, target_out_1)
+
+        return {
+            "loss_i": loss_i,
+            "loss_j": loss_j,
+            "mom": self.mom,
+            "loss_linear": linear_loss,
+            "acc@1": acc1,
+            "acc@5": acc5,
+        }
---
+++ examples/byol/BYOL.res50.imagenet.1024bsx2nodes.224size.100e.lars.online_linear/config.py
@@ -0,0 +1,120 @@
+import os.path as osp
+import torchvision.transforms as transforms
+
+from cvpods.configs.base_classification_config import BaseClassificationConfig
+
+_config_dict = dict(
+    MODEL=dict(
+        WEIGHTS="",
+        AS_PRETRAIN=True,
+        RESNETS=dict(
+            DEPTH=50,
+            NUM_CLASSES=1000,
+            NORM="BN",
+            OUT_FEATURES=["res5"],
+            STRIDE_IN_1X1=False,  # default true for msra models
+            ZERO_INIT_RESIDUAL=True,  # default false, use true for all subsequent models
+        ),
+        BYOL=dict(
+            BASE_MOMENTUM=0.99,
+            PROJ_DEPTH=2,
+            PROJ_DIM=4096,
+            OUT_DIM=256,
+        ),
+    ),
+    DATASETS=dict(
+        TRAIN=("imagenet_nori_train", ),
+        TEST=("imagenet_nori_val", ),
+    ),
+    DATALOADER=dict(NUM_WORKERS=6, ),
+    SOLVER=dict(
+        LR_SCHEDULER=dict(
+            NAME="WarmupCosineLR",
+            MAX_EPOCH=100,
+            WARMUP_ITERS=5,
+        ),
+        OPTIMIZER=dict(
+            NAME="LARS_SGD",
+            EPS=1e-8,
+            TRUST_COEF=1e-3,
+            CLIP=False,
+            # _LR_PRESETS = {40: 0.45, 100: 0.45, 300: 0.3, 1000: 0.2}
+            # _WD_PRESETS = {40: 1e-6, 100: 1e-6, 300: 1e-6, 1000: 1.5e-6}
+            # _EMA_PRESETS = {40: 0.97, 100: 0.99, 300: 0.99, 1000: 0.996}
+            BASE_LR=0.45 * 4,  # 0.3 for bs 256 => 4.8 for 4096
+            MOMENTUM=0.9,
+            WEIGHT_DECAY=1e-6,
+            WD_EXCLUDE_BN_BIAS=True,
+        ),
+        CHECKPOINT_PERIOD=10,
+        IMS_PER_BATCH=1024,
+        IMS_PER_DEVICE=128,  # 8 gpus per node
+        BATCH_SUBDIVISIONS=1,  # Simulate Batch Size 4096
+    ),
+    INPUT=dict(
+        AUG=dict(
+            TRAIN_PIPELINES=dict(
+                q=[
+                    ("RepeatList", dict(transforms=[
+                        ("Torch_Compose", transforms.Compose([
+                            transforms.RandomResizedCrop(224),
+                            transforms.RandomHorizontalFlip(),
+                            transforms.RandomApply([
+                                        transforms.ColorJitter(0.4, 0.4, 0.2, 0.1)], p=0.8),
+                            transforms.RandomGrayscale(p=0.2),
+                        ])),
+                        ("RandomGaussianBlur", dict(sigma=[.1, 2.], p=1.0)),
+                        ("RandomSolarization", dict(p=0.0)),
+                        ("Torch_Compose", transforms.Compose([
+                            transforms.ToTensor(),
+                            transforms.Normalize(
+                                mean=[0.485, 0.456, 0.406],
+                                std=[0.229, 0.224, 0.225])
+                        ])),
+                    ], repeat_times=1)),
+                ],
+                k=[
+                    ("RepeatList", dict(transforms=[
+                        ("Torch_Compose", transforms.Compose([
+                            transforms.RandomResizedCrop(224, scale=(0.08, 1.)),
+                            transforms.RandomHorizontalFlip(),
+                            transforms.RandomApply([
+                                        transforms.ColorJitter(0.4, 0.4, 0.2, 0.1)], p=0.8),
+                            transforms.RandomGrayscale(p=0.2),
+                        ])),
+                        ("RandomGaussianBlur", dict(sigma=[.1, 2.], p=0.1)),
+                        ("RandomSolarization", dict(p=0.2)),
+                        ("Torch_Compose", transforms.Compose([
+                            transforms.ToTensor(),
+                            transforms.Normalize(
+                                mean=[0.485, 0.456, 0.406],
+                                std=[0.229, 0.224, 0.225])
+                        ])),
+                    ], repeat_times=1)),
+                ],
+                linear=[
+                    ("Torch_Compose", transforms.Compose([
+                        transforms.RandomResizedCrop(224),
+                        transforms.RandomHorizontalFlip(),
+                        transforms.ToTensor(),
+                        transforms.Normalize(
+                            mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225]
+                        ),
+                    ])),
+                ],
+            )
+        )),
+    TRAINER=dict(FP16=dict(ENABLED=False, OPTS=dict(OPT_LEVEL="O1"))),
+    OUTPUT_DIR=osp.join(
+        '/data/Outputs/model_logs/cvpods_playground',
+        osp.split(osp.realpath(__file__))[0].split("playground/")[-1]))
+
+
+class MoCoV2Config(BaseClassificationConfig):
+    def __init__(self):
+        super(MoCoV2Config, self).__init__()
+        self._register_configuration(_config_dict)
+
+
+config = MoCoV2Config()
